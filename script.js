88df0abcb50c2ab8df64aa133fb8599554271ac6
bhatisahil01 <sahilbhati7060@gmail.com>
2024-01-11 17:38:51 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new typed(".multiple-text",{
+    strings:["Software Engineer","Java Full Stack Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop: true
+})
